e8c0ec4e41f8d7386cf9c2e9e52d6db6fb121431
riquee <strikedv@hotmail.com>
2022-07-17 10:38:11 -0300

    refactor: index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import LimpaGrid from './services/limpadgrid';
 import { CronJob } from 'cron';
 import { Cron } from './utils/crontab';
 
+console.log('X-POST LIMPAGRID START');
+
 const cron = new CronJob(Cron.EVERY_30_MINUTE, async () => {
   const limpagrid = new LimpaGrid();
   limpagrid.init();
